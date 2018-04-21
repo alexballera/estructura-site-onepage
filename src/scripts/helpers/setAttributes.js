9b24e51744cbc1eb9bc09715dcb5d60269e304ba
Alex Ballera <alexballera@gmail.com>
2018-04-21 13:06:28 -0400

    Update 2018-04-21T17:06:24.996Z

--- src/scripts/helpers/setAttributes.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-var setAttributes = (el, attrs) => {
-  for (var key in attrs) {
-    el.setAttribute(key, attrs[key])
-  }
-}
-
-export {setAttributes}
--- src/views/home/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-import {createHome} from './createHome'
-import Section1 from './section1'
-
-export default () => {
-  createHome()
-  Section1()
-}
